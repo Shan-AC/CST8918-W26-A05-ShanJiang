f34c03c9acf982668301fd7d849e57c236f7ee3d
Shan-AC <jian0147@algonquinlive.com>
2026-06-12 22:09:09 -0400

    feat: define all azure resources and cloudinit

--- main.tf
+++ main.tf
@@ -24,3 +24,117 @@ provider "azurerm" {
 provider "cloudinit" {
   # Configuration options
 }
+########################################
+# 4.1 Variables
+########################################
+variable "labelPrefix" {
+  type        = string
+  description = "Algonquin College"
+}
+
+variable "region" {
+  type    = string
+  default = "canadacentral" 
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureadmin"
+}
+########################################
+# 4.2 Resource Group
+########################################
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
+
+########################################
+# 4.3 Public IP Address
+########################################
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.labelPrefix}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+########################################
+# 4.4 Virtual Network
+########################################
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+########################################
+# 4.5 Subnet
+########################################
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+########################################
+# 4.6 Security Group
+########################################
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+########################################
+# 4.7 Virtual Network Interface Card (NIC)
+########################################
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.labelPrefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+########################################
+# 4.8 Apply the security group
+########################################
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
+
+
